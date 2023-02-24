263a54b4cd92a6f5e8683c3dffe6373f2e8fb474
AlexTroF <djjim977@mail.ru>
2023-02-24 23:39:22 +0300

    доработал оптимизацию очень больших изображений

--- src/lib/getPicturesList/getPicturesList.js
+++ src/lib/getPicturesList/getPicturesList.js
@@ -5,31 +5,38 @@ const compression = (dir_no_compression) => {
     dir_no_compression.forEach((picture, index) => {
         sharp(`public/pictures/no_compression/${picture}`)
             .resize({ width: 500 })
+            .toFile(`public/pictures/compression/preview/${picture}`)
+            .then(() => { })
+    })
+
+    dir_no_compression.forEach((picture, index) => {
+        sharp(`public/pictures/no_compression/${picture}`)
+            .resize({ width: 2500 })
             .toFile(`public/pictures/compression/${picture}`)
             .then(() => { })
     })
-    return fs.readdirSync('public/pictures/compression/')
 }
 
 export const getPicturesList = async (ctx) => {
     let dir_no_compression = fs.readdirSync('public/pictures/no_compression/');
-    let dir_compression = fs.readdirSync('public/pictures/compression/');
+    let dir_preview;
+    let dir_compression;
+
     let picturesList;
 
-    if (dir_no_compression.length !== dir_compression.length) {
-        dir_compression = compression(dir_no_compression);
-    }
+    compression(dir_no_compression);
 
-    picturesList = dir_no_compression.map(fileName => {
+    dir_preview = fs.readdirSync('public/pictures/compression/preview/');
+    dir_compression = fs.readdirSync('public/pictures/compression/');
+
+
+    picturesList = dir_compression.map(fileName => {
         return {
             fileName,
-            src: '/pictures/no_compression/' + fileName,
-            srcCompress: '/pictures/compression/' + fileName
-            // stats,
-            // fileData: file,
+            src: '/pictures/compression/' + fileName,
+            srcCompress: '/pictures/compression/preview/' + fileName
         }
     })
-    // console.log(dir);
     return {
         picturesList
     };
